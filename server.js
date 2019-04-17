6926ed148e640646cfefa5cba9db52c8266cc900
Xiong Moua <xiongmoua@hotmail.com>
2019-04-16 21:22:29 -0500

    created hello world and installed express

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// ES6 ?
+// import express from 'express';
+// import morgan from 'morgan';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8080);
