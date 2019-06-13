0ef3238b48a53b699a59469b4215b2e6e477c30c
Xiong Moua <xiongmoua@hotmail.com>
2019-06-13 16:39:09 -0500

    updated config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL
+exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
-exports.PORT = process.env.PORT
+exports.PORT = process.env.PORT || 5000;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api';
