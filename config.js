9518e53868f4c04d3a580c11c860cae465781ac1
Xiong Moua <xiongmoua@hotmail.com>
2019-06-13 14:21:25 -0500

    testing secret

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/chore-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || secret;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api';
